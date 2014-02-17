d447ca95b2b3f6f6b775a8ec49d3073579887427
Ctag <ctag@INN0095_VM3.(none)>
2014-02-17 18:34:49 +0100

    correxido o erro do cambio de line

--- src/its/app/spat/sender/utils/SpatGenerator.java
+++ src/its/app/spat/sender/utils/SpatGenerator.java
@@ -7,6 +7,7 @@ import its.fac.messages.api.types.IntersectionState;
 import its.fac.messages.api.types.MovementState;
 import its.fac.messages.api.types.Spat;
 import java.nio.ByteBuffer;
+import java.util.List;
 import java.util.Vector;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
@@ -65,7 +66,7 @@ public class SpatGenerator extends Thread {
 			boolean pedestrian=false;
 			for (int i = 0; i < strTLTopo.length; i++) {
 
-				//System.out.println(strTLTopo+" strTLTopo");
+				//System.out.println(strTLTopo[i]+" strTLTopo");
 				lon=strTLTopo[i].split(";").length;
 				if (lon==4){pedestrian=false;
 				arrayTLtopo[i]=Integer.parseInt(strTLTopo[i].split(";")[0]);	//grupo de semaforo; lineas separadas por comas;{color tipo}
@@ -101,143 +102,140 @@ public class SpatGenerator extends Thread {
 			typeTemp element;
 			int num= th1.List_temp.size();int i=0;
 			if(th1.waitingResponse)th1.Temp100=th1.Temp100-(Integer.parseInt(Activator.spatFrequency)/100); if(th1.waitingACK)th1.Temp10=th1.Temp10-(Integer.parseInt(Activator.spatFrequency)/100);
-			System.out.println(th1.Temp10+" "+th1.Temp100+ " temporizo");
 			
 			if(th1.Temp10<=0){th1.close();th1.Temp10=20;}else
 			if(th1.Temp100<=0){th1.close();th1.Temp100=200;}
+			List<typeTemp> clone1=th1.List_temp;
+			List<Integer> clone2=th1.List_ID;
 			while(i<num){
-				element=th1.List_temp.get(i);
+				element=clone1.get(i);
 				element.Timer_last=element.Timer_last - (Integer.parseInt(Activator.spatFrequency)/100);
 				if(element.Timer_last<=0){
-					th1.List_temp.remove(i);
-					th1.List_ID.remove(i);
-				}else th1.List_temp.set(i,element);
-				i++;
-				num=th1.List_temp.size();
+					clone1.remove(i);
+					clone2.remove(i);
+				}else{ clone1.set(i,element);
+				i++;}
+				num=clone1.size();
 			}
+			th1.List_temp=clone1;
+			th1.List_ID=clone2;
 			//System.out.println(th1.List_temp.size()+ "tamaño lista de temporización");
-			int val=0;
-			for ( i = 0; i < th1.List_temp.size(); i++) {
+			int[] arrayTL=new int[clone1.size()];
+			for ( i = 0; i < clone1.size(); i++) {
 
-				response[i][0]=(byte)(th1.List_temp.get(i).ID);
-				response[i][1]=th1.List_temp.get(i).color;//System.out.println("color "+message[4+10 + next_pos]);
+				response[i][0]=(byte)(clone1.get(i).ID);
+				response[i][1]=clone1.get(i).color;//System.out.println("color "+message[4+10 + next_pos]);
 				byte[] res=new byte[4];
-				res=ByteBuffer.allocate(4).putInt(th1.List_temp.get(i).Timer_last).array();
+				res=ByteBuffer.allocate(4).putInt(clone1.get(i).Timer_last).array();
 				response[i][2]=res[2];//System.out.println("veo2 "+res[2]+" "+res[3]);
 				response[i][3]=res[3];
-				arrayTLtopo[i]=(int)response[i][0]&0xff;//System.out.println("arraytltopo"+arrayTLtopo[i]);
-				val=i;
-
-			}
-			int[] x=arrayTLtopo.clone();
-			arrayTLtopo=new int[val+1];
-			for (int j = 0; j < (val+1); j++) {
-				arrayTLtopo[j]=x[j];
+				arrayTL[i]=(int)response[i][0]&0xff;//System.out.println("arraytltopo"+arrayTLtopo[i]);
 			}
 			if (response==null){			System.out.println("No conection" );return null;}
 			//	System.out.println("regreso de sending" +" "+0x03+" "+ response[0][0]+" "+response[0][1]);//+" "+response[1][0]);
-			int tlLengh = strTLTopo.length;
+			//int tlLengh = strTLTopo.length;
 			spat = itsMessagesSenderService.createSpat();
 			spat.setMsdId(DSRCMessageID.SIGNALPHASEANDTIMINGMESSAGE);
 			intersectionstate = spat.createIntersectionState(1);//está bien
 			intersectionstate.get(0).setIdIntersectionState(intersectionId);
 			intersectionstate.get(0).setTimeStamp(58); // No hace falta cubrirlo, sino poner la hora de la notificación
 			intersectionstate.get(0).setIntersectionStatus(0); // estados de 0 a 7
-			movementstate = intersectionstate.get(0).createMovementState(arrayTLtopo.length);
+			movementstate = intersectionstate.get(0).createMovementState(arrayTL.length);
 			long colortl1=0;
 			//	System.out.println("lonxitudes "+strTLTopo.length+" "+arraytype.length);
-			for ( i = 0; i < arrayTLtopo.length; i++) {
+			for (int ii = 0; ii < arrayTL.length; ii++) {
 			//	System.out.println(arrayTLtopo[i] + " "+ laneset[i][0]);
+				i=clone1.get(ii).ID -1;
 				if (laneset[i][0]>=0)	{			
 					String[] tls = arraytype[i].split(",");
 					String color = tls[0];
 					String type_len = tls[1];
 					String[] colorSetsString = color.split(":");
-					char[][] colorset = new char[arrayTLtopo.length][4];
+					char[][] colorset = new char[arrayTL.length][4];
 					String[] typeSetsString = type_len.split(":");
-					String[][] type_set = new String[arrayTLtopo.length][3];
-					colorset[i][0]= colorSetsString[0].charAt(2);//porque leva {(diante
-					colorset[i][1]= colorSetsString[1].charAt(0);
-					colorset[i][2]= colorSetsString[2].charAt(0);
-					type_set[i][0]= typeSetsString[0].substring(1);
-					type_set[i][1]= typeSetsString[1];
-					type_set[i][2]= typeSetsString[2].substring(0, typeSetsString[2].length()-2);
+					String[][] type_set = new String[arrayTL.length][3];
+					colorset[ii][0]= colorSetsString[0].charAt(2);//porque leva {(diante
+					colorset[ii][1]= colorSetsString[1].charAt(0);
+					colorset[ii][2]= colorSetsString[2].charAt(0);
+					type_set[ii][0]= typeSetsString[0].substring(1);
+					type_set[ii][1]= typeSetsString[1];
+					type_set[ii][2]= typeSetsString[2].substring(0, typeSetsString[2].length()-2);
 					int column=0;	int column1=1;	int aa=0;	int row=0;		int pos=0;
-					if(response[i][1]== 'D'){ colortl1=0x0;}else{
-						if(response[i][1]== 'V'|| response[i][1]== 'C'|| response[i][1]== 'P'){
-							aa=0;
-							column=check_colour(colorset[i][2]);
-							if(response[i][1]== 'C'|| response[i][1]== 'P'){aa=1;column1=check_colour(colorset[i][3]);}
+					if(response[ii][1]== 'D'){ colortl1=0x0;}else{
+						if(response[ii][1]== 'V'|| response[ii][1]== 'C'|| response[ii][1]== 'P'){
+							
+							column=check_colour(colorset[ii][2]);
+							if(response[ii][1]== 'C'|| response[ii][1]== 'P'){aa=1;column1=check_colour(colorset[ii][3]);}
 							if (column==0)pos=2;else if (column==2)pos=0; else pos=column;
-							row=check_type(type_set[i][pos]);System.out.println("Entrei en V " +column+ " "+row);
+							row=check_type(type_set[ii][pos]);System.out.println("Entrei en V " +column+ " "+row);
 
 						}
-						else if(response[i][1]== 'A' || response[i][1]== 'F'|| response[i][1]== 'N'|| response[i][1]== 'J'|| response[i][1]== 'I'|| response[i][1]== 'G'|| response[i][1]== 'S'|| response[i][1]== 'E'|| response[i][1]== 'K'|| response[i][1]== 'Z')
-						{aa=0;
-						column=check_colour(colorset[i][1]);
-						if(response[i][1]== 'E'|| response[i][1]== 'K'|| response[i][1]== 'F'|| response[i][1]== 'Z'||response[i][1]== 'J'|| response[i][1]== 'I'|| response[i][1]== 'G')
-						{column1=check_colour(colorset[i][3]);aa=1;}
-						if(response[i][1]== 'N'){column1=check_colour(colorset[i][2]);aa=1;}
-						if(response[i][1]== 'S')	{column1=check_colour(colorset[i][0]);aa=1;}
+						else if(response[ii][1]== 'A' || response[ii][1]== 'F'|| response[ii][1]== 'N'|| response[ii][1]== 'J'|| response[ii][1]== 'I'|| response[ii][1]== 'G'|| response[ii][1]== 'S'|| response[ii][1]== 'E'|| response[ii][1]== 'K'|| response[ii][1]== 'Z')
+						{
+						column=check_colour(colorset[ii][1]);
+						if(response[ii][1]== 'E'|| response[ii][1]== 'K'|| response[ii][1]== 'F'|| response[ii][1]== 'Z'||response[ii][1]== 'J'|| response[ii][1]== 'I'|| response[ii][1]== 'G')
+						{column1=check_colour(colorset[ii][3]);aa=1;}
+						if(response[ii][1]== 'N'){column1=check_colour(colorset[ii][2]);aa=1;}
+						if(response[ii][1]== 'S')	{column1=check_colour(colorset[ii][0]);aa=1;}
 						if (column==0)pos=2;else if (column==2)pos=0; else pos=column;
-						row=check_type(type_set[i][pos]);System.out.println("Entrei en A " +column+ " "+row);
+						row=check_type(type_set[ii][pos]);System.out.println("Entrei en A " +column+ " "+row);
 
-						}else if(response[i][1]== 'R'|| response[i][1]== 'B'|| response[i][1]== 'H'){
-							aa=0;
-							column=check_colour(colorset[i][0]);
-							if(response[i][1]== 'B'|| response[i][1]== 'H'){column1=check_colour(colorset[i][3]);aa=1;}
+						}else if(response[ii][1]== 'R'|| response[ii][1]== 'B'|| response[ii][1]== 'H'){
+				
+							column=check_colour(colorset[ii][0]);
+							if(response[ii][1]== 'B'|| response[ii][1]== 'H'){column1=check_colour(colorset[ii][3]);aa=1;}
 							if (column==0)pos=2;else if (column==2)pos=0; else pos=column;
-							row=check_type(type_set[i][pos]);System.out.println("Entrei en R " +column+ " "+row);
+							row=check_type(type_set[ii][pos]);System.out.println("Entrei en R " +column+ " "+row);
 						} 
 
-						if(response[i][1]== 'F'|| response[i][1]== 'J'|| response[i][1]== 'I'|| response[i][1]== 'G'|| response[i][1]== 'E'|| response[i][1]== 'K'|| response[i][1]== 'Z'){column=3;}
+						if(response[ii][1]== 'F'|| response[ii][1]== 'J'|| response[ii][1]== 'I'|| response[ii][1]== 'G'|| response[ii][1]== 'E'|| response[ii][1]== 'K'|| response[ii][1]== 'Z'){column=3;}
 						if (aa==1)	colortl1=kml[row][column]+kml[row][column1];
 						else colortl1=kml[row][column];         }
 				}else{
-					if(response[i][1]== 'D'||response[i][1]== 'C'||response[i][1]== 'F'||response[i][1]== 'B') colortl1=0x0;
-					else if(response[i][1]== 'V'|| response[i][1]== 'N'|| response[i][1]== 'P'|| response[i][1]== 'H')colortl1=0x1;
-					else if(response[i][1]== 'R'|| response[i][1]== 'S')colortl1=0x4;	
+					if(response[ii][1]== 'D'||response[ii][1]== 'C'||response[ii][1]== 'F'||response[ii][1]== 'B') colortl1=0x0;
+					else if(response[ii][1]== 'V'|| response[ii][1]== 'N'|| response[ii][1]== 'P'|| response[ii][1]== 'H')colortl1=0x1;
+					else if(response[ii][1]== 'R'|| response[ii][1]== 'S')colortl1=0x4;	
 
 				}
 				//System.out.println("rellena laneset");
-				movementstate.get(i).setMovementName("STATE"+"+i+");// no es necesario
+				movementstate.get(ii).setMovementName("STATE"+"+i+");// no es necesario
 				/**for (int j = 0; j < laneset.length; j++) {
 					System.out.println(laneset[j] );
 				}**/
 				//
 
-				int a=1;int ii=0;
+				int a=1;int ia=0;
 
 				while(a==1){
-					if(laneset[i][ii]==0)a=0;
-					ii++;
+					if(laneset[i][ia]==0)a=0;
+					ia++;
 				}
-				int [] laneset1=new int[ii-1]; a=1; ii=0;
-				while(ii<laneset1.length){
-					laneset1[ii]=laneset[i][ii];
-					ii++;
+				int [] laneset1=new int[ia-1]; a=1; ia=0;
+				while(ia<laneset1.length){
+					laneset1[ia]=laneset[i][ia];
+					ia++;
 				}		
 				for (int j = 0; j < laneset1.length; j++) {
 			//		System.out.println(i+" laneset1 "+laneset1[j]+" "+laneset1.length);
 				}
 				//
-				movementstate.get(i).setLaneSet(laneset1);
-				movementstate.get(i).setCurrState(colortl1);
+				movementstate.get(ii).setLaneSet(laneset1);
+				movementstate.get(ii).setCurrState(colortl1);
 				//System.out.println("tiempos "+response[i][2]+" "+ response[i][3]+" "+ response[i][4]+" "+ response[i][5]);
-				byte[] b={response[i][2],response[i][3]}; //correcto para os casos que non sexa -1, como se faría noutro caso??
-				int sum1=response[i][3]&(0xff);
-				int sum2= response[i][2]&(0xff);
+				byte[] b={response[ii][2],response[ii][3]}; //correcto para os casos que non sexa -1, como se faría noutro caso??
+				int sum1=response[ii][3]&(0xff);
+				int sum2= response[ii][2]&(0xff);
 				int sum=sum1+sum2*256;
 
 				//System.out.println(str);
-				if ( response[i][3]!=-1)movementstate.get(i).setTimeToChange(sum);else{
-					byte [] c={response[i][6],response[i][7]}; 
-					sum1=response[i][7]&(0xff);
-					sum2= response[i][6]&(0xff);
+				if ( response[i][3]!=-1)movementstate.get(ii).setTimeToChange(sum);else{
+					byte [] c={response[ii][6],response[ii][7]}; 
+					sum1=response[ii][7]&(0xff);
+					sum2= response[ii][6]&(0xff);
 					sum=sum1+sum2*256;
-					movementstate.get(i).setTimeToChange(sum);
+					movementstate.get(ii).setTimeToChange(sum);
 				}
-				System.out.println("time to change:"+sum+ " color:"+colortl1+ " laneset1:"+laneset1[0]);
+				System.out.println("Eu calculo: time to change:"+sum+ " color:"+colortl1+ " laneset1:"+laneset1[0]+" grupo semafórico;"+i);
 				//	System.out.println("Colors: " + colortl1 );
 				//	System.out.println("Time: " + sum);
 
